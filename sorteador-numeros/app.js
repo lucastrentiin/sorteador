aedf4099317e079517e77cfa3ed47a25cad2a7e0
lucastrentiin <lucastrentin222@gmail.com>
2025-01-07 04:44:50 -0300

    app.js

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -8,12 +8,38 @@ function sortear() { //começamos pelo sortar pois ao clicar nele ele puxa essas
 
     for (let i = 0; i < quantidadeNumero; i++) {
         numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
         sorteados.push(numero);
     }
 
- alert(sorteados);
+let resultado = document.getElementById('resultado');
+resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`;
+alterarStatusBotao();
 }
+
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar'); //peguei id do botão para sinalizar que la vai ser a alteração
+    if (botao.classList.contains('container__botao-desabilitado')) { // Se a classe contiver container__botao-desabilitado 
+        botao.classList.remove('container__botao-desabilitado');// Remove uma classe
+        botao.classList.add('container__botao');// Adiciona uma classe
+    } else {
+        botao.classList.remove('container__botao');// Remove uma classe
+        botao.classList.add('container__botao-desabilitado');// Adiciona uma classe
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
